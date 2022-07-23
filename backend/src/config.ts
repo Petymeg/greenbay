3bd74e178745c370227bde6e83af63fa53ad2b36
Virag Tamas <petymeg@gmail.com>
2022-07-23 10:34:23 +0200

    config file added

---
+++ backend/src/config.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  mysql: {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  },
+  servers: {
+    host: 'http://localhost',
+    port: process.env.PORT,
+  },
+  jwt: { secretKey: process.env.JWT_SECRET },
+};
