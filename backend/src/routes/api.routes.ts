4397f622e971d3df80a4f10fa5490494f9337593
Virag Tamas <petymeg@gmail.com>
2022-07-23 10:45:25 +0200

    Base app ts files added

---
+++ backend/src/routes/api.routes.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import cors from 'cors';
+
+const apiRouter = express.Router();
+
+apiRouter.use(cors());
+apiRouter.use(express.json());
+
+export default apiRouter;
