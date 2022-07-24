ad0715e2b3419d4db22878d445ab53093243b62f
Virag Tamas <petymeg@gmail.com>
2022-07-24 11:53:37 +0200

    DeleteProduct method added to productService

--- backend/src/services/productService.ts
+++ backend/src/services/productService.ts
@@ -1,5 +1,6 @@
 import { AddUserProductRequestModel } from '../models/common/AddUserProductRequestModel';
 import { productRepository } from '../repositories/product.repository';
+import { notFoundError, unauthorizedError } from './generalErrorService';
 import { userService } from './userService';
 
 export const productService = {
@@ -9,5 +10,18 @@ export const productService = {
     await userService.getUserById(productDetails.userId);
 
     return await productRepository.addUserProduct(productDetails);
+  },
+
+  async deleteProduct(productId: number, userId: number): Promise<void> {
+    const productDetails = await productRepository.getProductById(productId);
+
+    if (!productDetails) throw notFoundError('Product with this ID not found');
+
+    if (userId !== productDetails.userId)
+      throw unauthorizedError(
+        "You can't delete this product, it doesn't belong to you!"
+      );
+
+    await productRepository.deleteProductById(productId);
   },
 };
