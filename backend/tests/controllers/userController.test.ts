258bb8a0b52c041960f08029423c466297f94b8f
Virag Tamas <petymeg@gmail.com>
2022-08-02 14:28:16 +0200

    userController.login() tests added

--- backend/tests/controllers/userController.test.ts
+++ backend/tests/controllers/userController.test.ts
@@ -76,6 +76,8 @@ describe('userController.register()', () => {
       .send(registrationData);
 
     //Assert
+    expect(userService.register).toHaveBeenCalledWith('UserName', '12345678');
+    expect(userService.register).toHaveBeenCalledTimes(1);
     expect(result.statusCode).toEqual(500);
   });
 
@@ -100,6 +102,86 @@ describe('userController.register()', () => {
     expect(userService.register).toHaveBeenCalledWith('UserName', '12345678');
     expect(userService.register).toHaveBeenCalledTimes(1);
     expect(result.statusCode).toEqual(201);
+    expect(result.body).toEqual(userData);
+  });
+});
+
+describe('userController.login()', () => {
+  beforeEach(() => {
+    console.error = jest.fn();
+  });
+
+  it('Error code 400 when no userdata is provided', async () => {
+    //Arrange
+    //Act
+    const result = await request(app).post('/api/user/login').send({});
+
+    //Assert
+    expect(result.statusCode).toEqual(400);
+  });
+
+  it('Error code 400 when password is not provided', async () => {
+    //Arrange
+    const loginData = {
+      username: 'UserName',
+    };
+
+    //Act
+    const result = await request(app).post('/api/user/login').send(loginData);
+
+    //Assert
+    expect(result.statusCode).toEqual(400);
+  });
+
+  it('Error code 400 when username is not provided', async () => {
+    //Arrange
+    const loginData = {
+      password: '12345678',
+    };
+
+    //Act
+    const result = await request(app).post('/api/user/login').send(loginData);
+
+    //Assert
+    expect(result.statusCode).toEqual(400);
+  });
+
+  it('Error code 500 when service fails', async () => {
+    //Arrange
+    const loginData = {
+      username: 'UserName',
+      password: '12345678',
+    };
+    userService.login = jest.fn().mockRejectedValue('error');
+
+    //Act
+    const result = await request(app).post('/api/user/login').send(loginData);
+
+    //Assert
+    expect(userService.login).toHaveBeenCalledWith('UserName', '12345678');
+    expect(userService.login).toHaveBeenCalledTimes(1);
+    expect(result.statusCode).toEqual(500);
+  });
+
+  it('Proper object is sent when login is successful', async () => {
+    //Arrange
+    const loginData = {
+      username: 'UserName',
+      password: '12345678',
+    };
+    const userData = {
+      token: 'alsdfahskldjladc',
+      username: 'UserName',
+    };
+    userService.login = jest.fn().mockResolvedValue(userData);
+
+    //Act
+    const result = await request(app).post('/api/user/login').send(loginData);
+
+    //Assert
+    expect(userService.login).toHaveBeenCalledWith('UserName', '12345678');
+    expect(userService.login).toHaveBeenCalledTimes(1);
+    expect(result.statusCode).toEqual(200);
     expect(result.body).toEqual(userData);
   });
 });
