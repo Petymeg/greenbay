ae7a236b7f9ee57df30615e74532c199c98023df
Virag Tamas <petymeg@gmail.com>
2022-08-06 15:56:40 +0200

    Registration HTML and TS added

--- frontend/src/app/shared/shared/shared.module.ts
+++ frontend/src/app/shared/shared/shared.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, RouterModule],
-  exports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    CommonModule,
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
 })
 export class SharedModule {}
