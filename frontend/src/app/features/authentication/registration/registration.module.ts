d95970daf817186f746b0e9f1501aee91ebcd77a
Virag Tamas <petymeg@gmail.com>
2022-08-06 16:50:52 +0200

    CommonModule import removed from registration module

--- frontend/src/app/features/authentication/registration/registration.module.ts
+++ frontend/src/app/features/authentication/registration/registration.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RegistrationComponent } from './registration.component';
 import { RegistrationRoutingModule } from './registration-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [RegistrationComponent],
-  imports: [CommonModule, RegistrationRoutingModule, SharedModule],
+  imports: [RegistrationRoutingModule, SharedModule],
 })
 export class RegistrationModule {}
