8d49c5d9fef4dc19383dd22411e2b1341e7634f2
Virag Tamas <petymeg@gmail.com>
2022-08-06 15:19:59 +0200

    Not-found page added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./features/not-found/not-found.module').then(
+        (m) => m.NotFoundModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ frontend/src/app/features/not-found/not-found.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { NotFoundRoutingModule } from './not-found-routing.module';
+import { SharedModule } from 'src/app/shared/shared/shared.module';
+import { NotFoundComponent } from './not-found.component';
+
+@NgModule({
+  declarations: [NotFoundComponent],
+  imports: [SharedModule, NotFoundRoutingModule],
+})
+export class NotFoundModule {}
