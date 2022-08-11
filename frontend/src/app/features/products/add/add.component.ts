7cfbe2daa928a51514f403e373a2c424a35e9581
Petymeg <petymeg@gmail.com>
2022-08-11 12:08:13 +0200

    add component HTML and CSS structure added

--- frontend/src/app/features/products/add/add.component.ts
+++ frontend/src/app/features/products/add/add.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
-export class AddComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class AddComponent {
+  form = new FormGroup({
+    name: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    imgUrl: new FormControl('', Validators.required),
+    price: new FormControl('', Validators.required),
+  });
+  constructor() {}
 }
